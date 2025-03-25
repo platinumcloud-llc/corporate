0722ca5085046c44f64170ebbb4ee5294f8c6c76
なのくろ <nanocloudx@gmail.com>
2025-03-25 21:36:17 +0900

    add corporate info

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,17 @@ import {Services} from '@/components/Services'
 import {Contacts} from '@/components/Contacts'
 
 export const metadata = {
-  title: 'platinumcloud*',
-  description: 'テクノロジーを活用して、新しい "かわいい" を届けます。プラチナクラウド合同会社の企業サイトです。企業情報、事業情報、採用情報など。',
+  title: 'プラチナクラウド合同会社 (platinumcloud LLC)',
+  description: 'プラチナクラウド合同会社の企業サイトです。企業情報、事業情報、採用情報など。テクノロジーを活用して、新しい "かわいい" を届けます。',
   openGraph: {
-    title: 'platinumcloud*',
-    description: 'テクノロジーを活用して、新しい "かわいい" を届けます。プラチナクラウド合同会社の企業サイトです。企業情報、事業情報、採用情報など。',
+    title: 'プラチナクラウド合同会社 (platinumcloud LLC)',
+    description: 'プラチナクラウド合同会社の企業サイトです。企業情報、事業情報、採用情報など。テクノロジーを活用して、新しい "かわいい" を届けます。',
     url: 'https://platinumcloud.net',
     type: 'website',
     images: [
       {
         url: 'https://platinumcloud.net/platinumcloud.png',
-        alt: 'platinumcloud*',
+        alt: 'プラチナクラウド合同会社 (platinumcloud LLC)',
       },
     ],
   },
