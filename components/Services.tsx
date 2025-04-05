8c77053e85b63537a0ce28cf4f2e7da6a664d77f
なのくろ <nanocloudx@gmail.com>
2025-04-05 14:13:04 +0900

    public subscribe

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,6 @@
 import {Box, Heading, Text, VStack} from '@chakra-ui/react'
 import {SubscribeLogo} from '@/components/SubscribeLogo'
-// import Link from 'next/link'
+import Link from 'next/link'
 
 export function Services() {
   return (
@@ -15,42 +15,27 @@ export function Services() {
         px={5}
       >
         <Heading as={'h2'} fontSize={'2xl'} mb={8} textAlign={'center'}>事業・サービス</Heading>
-        {/*<Link href={'https://subscribe.sh/'} target={'_blank'}>*/}
-        {/*  <VStack*/}
-        {/*    p={14}*/}
-        {/*    borderRadius={10}*/}
-        {/*    bgColor={'#fff'}*/}
-        {/*    gap={4}*/}
-        {/*    className={'shadow'}*/}
-        {/*    transition={'transform .2s'}*/}
-        {/*    _hover={{*/}
-        {/*      transform: 'scale(1.03)',*/}
-        {/*      opacity: 0.9*/}
-        {/*    }}*/}
-        {/*    mb={10}*/}
-        {/*  >*/}
-        {/*    <SubscribeLogo size={'4xl'}/>*/}
-        {/*    <Text fontSize={'sm'} textAlign={'center'} color={'#555'}>*/}
-        {/*      表現する人と応援する人をつなぐ<br />スケジュール配信プラットフォーム <b>subscribe*</b>*/}
-        {/*    </Text>*/}
-        {/*  </VStack>*/}
-        {/*</Link>*/}
-        <VStack
-          p={14}
-          borderRadius={10}
-          bgColor={'#eee'}
-          gap={4}
-          opacity={0.7}
-          mb={10}
-        >
-          <SubscribeLogo size={'4xl'}/>
-          <Text fontSize={'sm'} textAlign={'center'} color={'#555'}>
-            表現する人と応援する人をつなぐ<br />スケジュール配信プラットフォーム <b>subscribe*</b>
-          </Text>
-          <Text textAlign={'center'} color={'#555'}>
-            まもなく公開予定
-          </Text>
-        </VStack>
+        <Link href={'https://subscribe.sh/'} target={'_blank'}>
+          <VStack
+            p={14}
+            borderRadius={10}
+            bgColor={'#fff'}
+            gap={4}
+            className={'shadow'}
+            transition={'transform .2s'}
+            _hover={{
+              transform: 'scale(1.03)',
+              opacity: 0.9
+            }}
+            mb={10}
+            border={'4px solid #bbe2f1'}
+          >
+            <SubscribeLogo size={'4xl'}/>
+            <Text fontSize={'sm'} textAlign={'center'} color={'#555'}>
+              表現する人と応援する人をつなぐ<br />スケジュール配信プラットフォーム <b>subscribe*</b>
+            </Text>
+          </VStack>
+        </Link>
 
         <VStack
           p={10}
@@ -63,7 +48,7 @@ export function Services() {
           <Text textAlign={'center'} color={'#555'} fontWeight={'bold'} fontSize={'lg'}>ウェブアプリ開発(業務委託)</Text>
           <Text textAlign={'center'} color={'#555'} fontSize={'sm'}>
             Next.js / React / TypeScript / GoogleCloud 等を組み合わせたWebアプリケーションの設計開発を承ります<br />
-            (現在は募集を休止しております)
+            (現在は募集休止しております)
           </Text>
         </VStack>
 
