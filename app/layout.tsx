1ee046106fd40e83f11e96e0c135e98c8a74506f
なのくろ <nanocloudx@gmail.com>
2025-04-11 09:17:51 +0900

    rm clientprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import {Capriola, Noto_Sans_JP, Roboto} from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
-import ClientProvider from '@/components/ClientProvider'
 import './global.css'
 import {Box} from '@chakra-ui/react'
 import {PlatinumCloudLogo} from '@/components/PlatinumCloudLogo'
 import {Footer} from '@/components/Footer'
+import {Provider} from '@/components/ui/provider'
 
 const capriola = Capriola({
   weight: ['400'],
@@ -27,14 +27,14 @@ export default function Layout({children}: Readonly<{ children: React.ReactNode 
       className={`${capriola.className} ${roboto.className} ${notoSansJP.className}`}
     >
       <body>
-        <ClientProvider>
+        <Provider>
           <Box minH={'100vh'}>
             <Box w={'full'} h={'7px'} className={'themeGradient'}/>
             <PlatinumCloudLogo/>
             {children}
             <Footer/>
           </Box>
-        </ClientProvider>
+        </Provider>
         <GoogleAnalytics gaId="G-0WTG5V6GLM" />
       </body>
     </html>
